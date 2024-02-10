e4d143d95a91420a91a44160efe172cc7ada798f
guimiiller <guilhermemillerblack@gmail.com>
2024-02-10 10:08:12 -0300

    remove tags a header

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -4,6 +4,7 @@ import Icon1 from '../../public/assets/icon_home.png'
 import Icon2 from  '../../public/assets/icon_about.png'
 import Icon3 from  '../../public/assets/icon_house.png'
 import Icon4 from  '../../public/assets/icon_contact.png'
+import Link from 'next/link'
 
 export const Header = () =>{
     return(
@@ -13,28 +14,28 @@ export const Header = () =>{
                     <div className={styles.nav_menu} id="nav-menu">
                         <ul className={styles.nav_list}>
                             <li className={styles.nav_item}>
-                                <a href="/" className={styles.nav_link}>
+                                <Link href="/" className={styles.nav_link}>
                                     <Image src={Icon1} alt="Icon 1" width={24} height={24} className={styles.icon_nav}/>
                                     <span className={styles.nav_name}>Home</span>
-                                </a>
+                                </Link>
                             </li>
                             <li className={styles.nav_item}>
-                                <a href="#houses" className={styles.nav_link}>
+                                <Link href="#houses" className={styles.nav_link}>
                                     <Image src={Icon3} alt="Icon 3" width={24} height={24} className={styles.icon_nav}/>
                                     <span className={styles.nav_name}>Imóveis</span>
-                                </a>
+                                </Link>
                             </li>
                             <li className={styles.nav_item}>
-                                <a href="#about" className={styles.nav_link}>
+                                <Link href="#about" className={styles.nav_link}>
                                     <Image src={Icon2} alt="Icon 2" width={24} height={24} className={styles.icon_nav}/>
                                     <span className={styles.nav_name}>Sobre</span>
-                                </a>
+                                </Link>
                             </li>
                             <li className={styles.nav_item}>
-                                <a href="#contact" className={styles.nav_link}>
+                                <Link href="#contact" className={styles.nav_link}>
                                     <Image src={Icon4} alt="Icon 4" width={24} height={24} className={styles.icon_nav}/>
                                     <span className={styles.nav_name}>Contato</span>
-                                </a>
+                                </Link>
                             </li>
                         </ul>
                     </div>
