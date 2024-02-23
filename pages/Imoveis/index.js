b23f321181b12e252c25fd1f9fcf0433392b7d3b
guimiiller <guilhermemillerblack@gmail.com>
2024-02-22 21:25:13 -0300

    add house 22, 23 and 24

--- pages/Imoveis/index.js
+++ pages/Imoveis/index.js
@@ -28,6 +28,9 @@ import House18_1 from '../../public/assets/houses/house18/img18_1.jpeg';
 import House19_1 from '../../public/assets/houses/house19/img19_1.jpeg';
 import House20_1 from '../../public/assets/houses/house20/img20_1.jpeg';
 import House21_1 from '../../public/assets/houses/house21/img21_1.jpeg';
+import House22_1 from '../../public/assets/houses/house22/img22_1.jpeg';
+import House23_1 from '../../public/assets/houses/house23/img23_1.jpeg';
+import House24_1 from '../../public/assets/houses/house24/img24_1.jpeg';
 
 const Imoveis = () => {
     const [bairroValue, setBairroValue] = useState('');
@@ -55,8 +58,11 @@ const Imoveis = () => {
         {id: 17, photo: House17_1, city: 'Limeira', district: 'Cond. Jaguarandi', price: 'R$ 1.000.000', category: 'chácara', rooms: 4, bathrooms: 3, meters: '1.600 m²'},
         {id: 18, photo: House18_1, city: 'Piracicaba', district: 'Santa Isabel', price: 'R$ 450.000,00', category: 'chácara', rooms: 2, bathrooms: 1, meters: '1.356 m²'},
         {id: 19, photo: House19_1, city: 'Limeira', district: 'Cond. Portal dos Eucaliptos', price: 'R$ 350.000,00', category: 'chácara', rooms: 3, bathrooms: 1, meters: '1.250 m²'},
-        {id: 20, photo: House20_1, city: 'Limeira', district: 'Jd. do lago', price: 'R$ 350.000,00', category: 'casa', rooms: 2, bathrooms: 2, meters: '186 m²'},
+        {id: 20, photo: House20_1, city: 'Limeira', district: 'Jd. do Lago', price: 'R$ 350.000,00', category: 'casa', rooms: 2, bathrooms: 2, meters: '186 m²'},
         {id: 21, photo: House21_1, city: 'Americana', district: 'Carioba', price: 'R$ 510.000,00', category: 'casa', rooms: 3, bathrooms: 1, meters: '175 m²'},
+        {id: 22, photo: House22_1, city: 'Americana', district: 'Nossa Senhora do Carmo', price: 'R$ 380.000,00', category: 'casa', rooms: 3, bathrooms: 1, meters: '300 m²'},
+        {id: 23, photo: House23_1, city: 'Nova Odessa', district: 'Jd. do Lago', price: 'R$ 320.000,00', category: 'casa', rooms: 1, bathrooms: 1, meters: '250 m²'},
+        {id: 24, photo: House24_1, city: 'Santa Barbara d Oeste', district: 'Mollon', price: 'R$ 600.000,00', category: 'casa', rooms: 3, bathrooms: 2, meters: '150 m²'},
     ];
 
     
